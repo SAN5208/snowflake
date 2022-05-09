df069695600c4ad3520ee89d09276c50553caab1
SAN5208 <104487867+SAN5208@users.noreply.github.com>
2022-05-09 06:37:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ my_cur.execute("select * from fruit_load_list")
 my_data_row=my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 streamlit.dataframe(my_data_row)
-add_to_fruit =raw_input("Enter Fruit you want to add:")
+add_to_fruit =text_input("Enter Fruit you want to add:")
 streamlit.dataframe.add(add_to_fruit)
 
