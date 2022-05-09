3a4752a3cdef7d1d3aab5be9a336b176775cee3f
SAN5208 <104487867+SAN5208@users.noreply.github.com>
2022-05-09 04:11:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 fruits_selected=streamlit.multiselect("pick some fruits",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]                                                                                     
 streamlit.dataframe(fruits_to_show)
-my_cnx= snowflake.connector.commect(**streamlit.secrets["snowflake"])
+my_cnx= snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur=my_cnx.cursor()
 
 
