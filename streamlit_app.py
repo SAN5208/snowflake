05ad8dfc88e46e76405c971b6f0028ca945dd125
SAN5208 <104487867+SAN5208@users.noreply.github.com>
2022-05-10 03:44:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,10 @@ def get_fruity_voice_data(this_fruit_choice):
         fruity_vice_response=requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
         fruity_vice_normalized=pandas.json_normalize(fruity_vice_response.json())
         return fruity_vice_normalized
-    
+def insert_row_snowflake(new_fruit):
+   with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")     
+        return "Thanks for adding" +new_fruit
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
@@ -26,18 +29,21 @@ try:
     fruit_choice =streamlit.text_input("Enter Fruit you want to add:")
     if not fruit_choice:
       streamlit.error("Please select fruit")
-    else:
-        back_from_function=get_fruity_voice_data(fruit_choice)
-        streamlit.dataframe(back_from_function)
+    elsif:
+                streamlit.button('Add a fruit to the list')
+                my_cnx= snowflake.connector.connect(**streamlit.secrets["snowflake"])
+                #back_from_function=get_fruity_voi(ce_data(fruit_choice)
+                back_from_function=insert_row_snowflake(fruit_choice)
+                streamlit.text(back_from_function)
+                #streamlit.dataframe(back_from_function)
 except url_error as e:
   streamlit.error()
  
 streamlit.stop()
                                                                                  
 streamlit.dataframe(fruits_to_show)
-my_cnx= snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
 my_cur=my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 my_data_row=my_cur.fetchall()
 streamlit.text("Hello from Snowflake:")
 
