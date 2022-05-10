aa5fc555986f2bb9433b547de8de2c3fa0980bd1
SAN5208 <104487867+SAN5208@users.noreply.github.com>
2022-05-10 03:55:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,5 @@ try:
                 back_from_function=insert_row_snowflake(fruit_choice)
                 streamlit.text(back_from_function)
                
- except:
+except:
      streamlit.error("Please check")   
